81f7d0c86b6be76d64b4b6506c59a498aec854d8
joshua stein <jcs@jcs.org>
2022-11-28 10:55:58 -0600

    Sponge: import from newer project, returns SpongeResponse objects

--- app/extras/sponge.rb
+++ app/extras/sponge.rb
@@ -1,10 +1,23 @@
 require "cgi"
 require "uri"
 require "net/https"
-require "resolv"
+require "socket"
 require "ipaddr"
 require "securerandom"
 require "stringio"
+
+require "active_support/hash_with_indifferent_access"
+
+class CaseInsensitiveHash < HashWithIndifferentAccess
+  def [](key)
+    super convert_key(key)
+  end
+
+protected
+  def convert_key(key)
+    key.respond_to?(:downcase) ? key.downcase : key
+  end
+end
 
 module Net
   class HTTP
@@ -34,25 +47,59 @@ module Net
   end
 end
 
-class SpongeError < StandardError; end
-class SpongeDNSError < StandardError; end
-class SpongeConnectError < StandardError; end
-class SpongeTimeoutError < StandardError; end
-class SpongeTLSError < StandardError; end
-class SpongeRedirectError < StandardError; end
+class SpongeResponse
+  attr_reader :from_uri
+
+  def initialize(net_http_res, from_uri = nil)
+    @res = net_http_res
+    @from_uri = from_uri
+  end
+
+  def inspect
+    "<#{self.class} from #{self.from_uri.to_s}: status=#{self.status} " <<
+      "body=#{self.body ? self.body.to_s[0, 100] : nil}>"
+  end
+
+  def body
+    @res.body
+  end
+
+  def status
+    @res.code.to_i
+  end
+
+  def headers
+    return @headers if @headers
+
+    @headers = CaseInsensitiveHash.new(@res.to_hash)
+    @headers.each do |k,v|
+      @headers[k] = v[0]
+    end
+    @headers
+  end
+
+  def json
+    @json ||= JSON.parse(@res.body)
+  end
+
+  def ok?
+    (200 .. 299).include?(status)
+  end
+
+  def to_s
+    @res.body
+  end
+end
 
 class Sponge
   MAX_TIME = 60
   MAX_DNS_TIME = 10
 
-  # options
-  attr_accessor :debug, :follow_redirection, :use_custom_resolver,
-    :keep_alive, :timeout, :use_private_keepalives, :avoid_badnets, :local_ip,
-    :resolve_cache, :user_agent
+  @@KEEP_ALIVES = {}
 
-  # results
-  attr_reader :used_keep_alive, :last_status, :last_response,
-    :final_permanent_redirection
+  attr_accessor :debug, :follow_redirection, :use_custom_resolver,
+    :keep_alive, :timeout, :use_private_keepalives, :resolve_cache,
+    :avoid_badnets, :local_ip, :user_agent
 
   # rfc3330
   BAD_NETS = [
@@ -70,9 +117,9 @@ class Sponge
   ]
 
   # old api
-  def self.fetch(url, headers = {}, limit = 10)
+  def self.fetch(uri, headers = {}, limit = 10)
     s = Sponge.new
-    s.fetch(url, "get", nil, nil, headers, {}, limit)
+    s.fetch(uri, "get", nil, nil, headers, {}, limit)
   end
 
   def initialize
@@ -84,7 +131,7 @@ class Sponge
     @use_private_keepalives = false
     @resolve_cache = {}
     @local_ip = nil
-    @used_keep_alive = false
+    @json = nil
     @user_agent = "sponge/1.0"
 
     @avoid_badnets = true
@@ -95,8 +142,31 @@ class Sponge
     rescue
     end
 
-    @keep_alives = {}
-    Thread.current[:sponge_keep_alives] ||= {}
+    @KEEP_ALIVES = {}
+  end
+
+  def find_keep_alive_for(host)
+    if self.use_private_keepalives
+      return @KEEP_ALIVES[host]
+    else
+      return @@KEEP_ALIVES[host]
+    end
+  end
+
+  def save_keep_alive(host, obj)
+    if self.use_private_keepalives
+      if obj == nil
+        @KEEP_ALIVES.delete(host)
+      else
+        @KEEP_ALIVES[host] = obj
+      end
+    else
+      if obj == nil
+        @@KEEP_ALIVES.delete(host)
+      else
+        @@KEEP_ALIVES[host] = obj
+      end
+    end
   end
 
   def set_cookie(from_host, cookie_line)
@@ -148,57 +218,41 @@ class Sponge
     end
   end
 
-  def fetch(url, method = :get, fields = nil, raw_post_data = nil,
+  def fetch(uri, method = :get, fields = nil, raw_post_data = nil,
   headers = {}, attachments = {}, limit = 10)
-    raise SpongeRedirectError, "Too many HTTP redirections" if limit <= 0
+    raise ArgumentError, "http redirection too deep" if limit <= 0
 
-    uri = nil
-    if url.is_a?(URI)
-      uri = url
-    else
-      uri = URI.parse(url)
+    if !uri.is_a?(URI)
+      uri = URI.parse(uri)
     end
-    if uri.host == nil
-      if url.to_s.match(/\A[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)\z/)
-        uri = URI.parse("http://#{url}")
-      end
-    end
-
-    if uri.host == nil
-      raise SpongeError, "invalid/incomplete URL #{url.to_s.inspect}"
-    elsif uri.path == ""
-      uri.path = "/"
-    end
-
-    uri.fragment = nil
-
     host = nil
     ip = nil
     method = method.to_s.downcase.to_sym
-    @used_keep_alive = false
+    @json = nil
 
-    if self.keep_alive && (host = self.find_keep_alive_for(uri))
+    if self.keep_alive && (host = self.find_keep_alive_for(uri.host))
       dputs "using cached keep-alive connection to #{uri.host}"
-      @used_keep_alive = true
     else
-      if self.use_custom_resolver
+      if @use_custom_resolver
         # we'll manually resolve the ip so we can verify it's not local
         tip = nil
-        ips = self.resolve_cache[uri.host]
+        ips = @resolve_cache[uri.host]
         if !ips || !ips.any?
           begin
             Timeout.timeout(MAX_DNS_TIME) do
               ips = [ Addrinfo.ip(uri.host).ip_address ]
 
               if !ips.any?
-                raise SpongeDNSError, "DNS: could not resolve " <<
-                  "#{uri.host.inspect}"
+                raise
               end
 
-              self.resolve_cache[uri.host] = ips
+              @resolve_cache[uri.host] = ips
             end
-          rescue SocketError, Timeout::Error
-            raise SpongeDNSError, "DNS: couldn't resolve #{uri.host}"
+          rescue Timeout::Error
+            raise "couldn't resolve #{uri.host} (DNS timeout)"
+          rescue SocketError, StandardError => e
+            raise "couldn't resolve #{uri.host} (#{e.inspect}) " <<
+              "(#{ips.inspect}) {#{tip.inspect})"
           end
         end
 
@@ -207,12 +261,12 @@ class Sponge
         ip = IPAddr.new(tip)
 
         if !ip
-          raise "DNS: couldn't resolve #{uri.host.inspect} to a usable IP"
+          raise "couldn't resolve #{uri.host}"
         end
 
-        if self.avoid_badnets &&
+        if @avoid_badnets &&
         BAD_NETS.select{|n| IPAddr.new(n).include?(ip) }.any?
-          raise SpongeDNSError, "DNS: refusing to talk to RFC3330 IP #{ip.to_s}"
+          raise "refusing to talk to IP #{ip.to_s}"
         end
 
         host = Net::HTTP.new(ip.to_s, uri.port)
@@ -232,26 +286,28 @@ class Sponge
       end
 
       if self.debug
-        host.set_debug_output $stdout
+        host.set_debug_output STDOUT
       end
 
       if uri.scheme == "https"
         host.use_ssl = true
-        #host.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        host.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
     end
 
-    path = (uri.path == "" ? "/" : uri.path)
-    if uri.query
-      path += "?" + uri.query
-    elsif method == :get && raw_post_data
-      path += "?" + URI.encode(raw_post_data)
-      if !headers["Content-Type"]
-        headers["Content-Type"] = "application/x-www-form-urlencoded"
+    # convert post params into query params for get requests
+    if method == :get
+      if raw_post_data
+        uri.query = URI.encode(raw_post_data)
+        if !headers["Content-Type"]
+          headers["Content-Type"] = "application/x-www-form-urlencoded"
+        end
+      elsif fields && fields.any?
+        uri.query = encode_fields(fields)
       end
     end
 
-    if method == :post
+    if method != :get
       if raw_post_data && attachments.any?
         raise "can't do raw POST data and attachments"
       end
@@ -261,15 +317,15 @@ class Sponge
 
         headers["Content-Type"] = "multipart/form-data; boundary=#{boundary}"
 
-        sio = StringIO.new("")
-        sio.binmode
-        sio.write fields.map{|k,v|
+        post_data = fields.map{|k,v|
           "--#{boundary}\r\n" +
           "Content-Disposition: form-data; name=\"#{k}\"\r\n" +
           "\r\n" +
           v.to_s +
           "\r\n"
         }.join
+
+				post_data = post_data.force_encoding("binary")
 
         attachments.each do |k,v|
           if !v.is_a?(Hash)
@@ -278,39 +334,42 @@ class Sponge
             raise "attachment #{k} has no :data"
           end
 
-          sio.write "--#{boundary}\r\n" +
-            "Content-Disposition: form-data; name=\"#{k}\"; filename=\"" +
-              "#{v[:filename]}\"\r\n" +
-            "Content-Type: #{v[:content_type]}\r\n" +
-            "\r\n"
+          post_data << ("--#{boundary}\r\n" <<
+            "Content-Disposition: form-data; name=\"#{k}\"; filename=\"" <<
+              "#{v[:filename]}\"\r\n" <<
+            "Content-Type: #{v[:content_type]}\r\n" <<
+            "\r\n").force_encoding("binary")
 
-          sio.write v[:data]
-          sio.write "\r\n"
+          post_data << v[:data].force_encoding("binary")
+          post_data << "\r\n".force_encoding("binary")
         end
 
-        sio.write "--#{boundary}--\r\n"
+        post_data << ("--#{boundary}--\r\n").force_encoding("binary")
 
-        sio.seek(0)
-        post_data = sio.read
-
+        post_data = post_data.force_encoding("binary")
       elsif raw_post_data
         post_data = raw_post_data
         if !headers["Content-Type"]
           headers["Content-Type"] = "application/x-www-form-urlencoded"
         end
+      elsif fields && fields.any?
+        post_data = encode_fields(fields)
       else
-        post_data = fields.map{|k,v|
-          "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}" }.join("&")
+        post_data = ""
       end
 
       headers["Content-Length"] = post_data.bytesize.to_s
     end
 
-    path.gsub!(/^\/\//, "/")
+    if uri.path.to_s == ""
+      uri.path = "/"
+    end
+
+    uri.path = uri.path.gsub(/^\/\/+/, "/")
 
     cooks = cookies(uri.host).to_s
 
-    dputs "fetching #{url} (#{ip.to_s}) " + (uri.user ? "with http auth " +
+    dputs "fetching #{uri} (#{ip.to_s}) " + (uri.user ? "with http auth " +
       uri.user + "/" + ("*" * uri.password.length) + " " : "") +
       "by #{method} with cookies #{cooks}" +
       (attachments.any? ? " with #{attachments.length} attachment(s)" : "")
@@ -333,31 +392,48 @@ class Sponge
 
     if uri.user
       headers["Authorization"] = "Basic " +
-        ["#{uri.user}:#{uri.password}"].pack('m').delete("\r\n")
+        ["#{uri.user}:#{uri.password}"].pack("m").delete("\r\n")
     end
 
     res = nil
     begin
-      Timeout.timeout(self.timeout) do
-        if method == :post
-          res = host.post(path, post_data, headers)
-        else
-          res = host.get(path, headers)
-        end
+      path = uri.path
+      if uri.query.to_s != ""
+        path += "?" + uri.query
       end
 
+      Timeout.timeout(@timeout) do
+        req = case method
+        when :delete
+          Net::HTTP::Delete.new(path, headers)
+        when :get
+          Net::HTTP::Get.new(path, headers)
+        when :options
+          Net::HTTP::Options.new(path, headers)
+        when :post
+          Net::HTTP::Post.new(path, headers)
+        when :put
+          Net::HTTP::Put.new(path, headers)
+        else
+          raise "unsupported method #{method}"
+        end
+
+        if post_data
+          req.body = post_data
+        end
+
+        res = host.request(req)
+      end
     rescue EOFError, Errno::EBADF => e
-      if self.keep_alive && self.used_keep_alive
+      if self.keep_alive && self.find_keep_alive_for(uri.host)
         # tried to re-use a dead connection, retry again from the start
-        self.save_keep_alive(uri, nil)
+        self.save_keep_alive(uri.host, nil)
         dputs "got eof using dead keep-alive socket, retrying"
-        return fetch(url, method, fields, raw_post_data, headers, attachments,
+        return fetch(uri, method, fields, raw_post_data, headers, attachments,
           limit - 1)
       else
         raise e
       end
-    rescue Timeout::Error
-      raise SpongeTimeoutError, "Timed out fetching from #{uri.host.inspect}"
     end
 
     if res.get_fields("Set-Cookie")
@@ -366,26 +442,17 @@ class Sponge
       end
     end
 
-    @last_response = res
-    @last_status = res.code.to_i
-
     if self.keep_alive
-      self.save_keep_alive(uri, host)
+      self.save_keep_alive(uri.host, host)
     end
 
-    case res.code.to_i
-    when 301, 302, 307, 308
-      if self.follow_redirection
-        begin
-          newuri = URI.parse(res["location"])
-        rescue URI::InvalidURIError => e
-          raise SpongeRedirectError,
-            "invalid URI #{res["location"].inspect} following " <<
-            "#{res.code} redirection from #{uri.inspect}"
-        end
-
+    case res
+    when Net::HTTPRedirection
+      if @follow_redirection
+        # follow
+        newuri = URI.parse(res["location"])
         if newuri.host
-          dputs "following redirection to #{res["location"]}"
+          dputs "following redirection to " + res["location"]
         else
           # relative path
           newuri.host = uri.host
@@ -393,87 +460,56 @@ class Sponge
           newuri.port = uri.port
           newuri.path = "/#{newuri.path}"
 
-          dputs "following relative redirection to #{newuri.to_s}"
-        end
-
-			  # moved permanently, remember it
-        if res.code.to_i == 301 &&
-			  (limit == 10 || self.final_permanent_redirection)
-				  @final_permanent_redirection = newuri.to_s
+          dputs "following relative redirection to " + newuri.to_s
         end
 
         fetch(newuri.to_s, "get", nil, nil, {}, {}, limit - 1)
       else
         dputs "not following redirection (disabled)"
-        return res.body
-      end
-    when 304
-      # not modified
-      return nil
-
-    else
-      return res.body
-    end
-  end
-
-  def get(url)
-    fetch(url, :get)
-  end
-
-  def post(url, fields)
-    fetch(url, :post, fields)
-  end
-
-protected
-  def keep_alive_key_for(uri)
-    "#{uri.scheme}://#{uri.host}"
-  end
-
-  def find_keep_alive_for(uri)
-    ka = nil
-
-    if self.use_private_keepalives
-      ka = @keep_alives[keep_alive_key_for(uri)]
-    else
-      ka = Thread.current[:sponge_keep_alives][keep_alive_key_for(uri)]
-    end
-
-    if ka
-      if ka[:time] >= (Time.now - 15)
-        return ka[:socket]
-      else
-        dputs "keep alive too stale, discarding"
-      end
-    end
-
-    nil
-  end
-
-  def save_keep_alive(uri, obj)
-    if self.use_private_keepalives
-      if obj == nil
-        @keep_alives.delete(keep_alive_key_for(uri))
-      else
-        @keep_alives[keep_alive_key_for(uri)] = {
-          :time => Time.now,
-          :socket => obj,
-        }
+        return SpongeResponse.new(res, uri)
       end
     else
-      if obj == nil
-        Thread.current[:sponge_keep_alives][keep_alive_key_for(uri)] = nil
-      else
-        Thread.current[:sponge_keep_alives][keep_alive_key_for(uri)] = {
-          :time => Time.now,
-          :socket => obj,
-        }
-      end
+      return SpongeResponse.new(res, uri)
     end
   end
 
+  def get(uri, params = {}, headers = {})
+    fetch(uri, :get, params, nil, headers)
+  end
+
+  def post(uri, fields, headers = {})
+    fetch(uri, :post, fields, nil, headers)
+  end
+
+private
   def dputs(string)
     if self.debug
       puts string
     end
+  end
+
+  def encode_fields(fields)
+    e = []
+    fields.each do |k,v|
+      if v.is_a?(Hash)
+        # :user => { :name => "hi", :age => "1" }
+        # becomes
+        # user[hame]=hi and user[age]=1
+        v.each do |vk,vv|
+          e.push "#{CGI.escape("#{k}[#{vk}]")}=#{CGI.escape(vv.to_s)}"
+        end
+      elsif v.is_a?(Array)
+        # :user => [ "one", "two" ]
+        # becomes
+        # user[]=one and user[]=two
+        v.each do |vv|
+          e.push "#{CGI.escape("#{k}[]")}=#{CGI.escape(vv.to_s)}"
+        end
+      else
+        e.push "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"
+      end
+    end
+
+    e.join("&")
   end
 end
