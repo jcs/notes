e3714a3e629ce1ca80b361b6c43c53bb029c5d7f
joshua stein <jcs@jcs.org>
2022-12-09 15:55:38 -0600

    app: On OpenBSD, pledge and unveil worker processes

---
+++ config/pledge_unveil.rb
@@ -0,0 +1,49 @@
+# this must be included in unicorn.conf.rb after forking, or unicorn won't be
+# able to gracefully restart
+#
+# after_fork do |server, worker|
+#   if defined?(ActiveRecord::Base)
+#     ActiveRecord::Base.establish_connection
+#   end
+#
+#   require "#{app_dir}/config/pledge_unveil.rb"
+# end
+
+if RUBY_PLATFORM.match(/openbsd/i)
+  require "unveil"
+
+  Pledge.unveil({
+    "#{App.root}/app" => "r",
+    "#{App.root}/config" => "r",
+    "#{App.root}/db" => "r",
+    "#{App.root}/db/production" => "rwc",
+    "#{App.root}/lib" => "r",
+    "#{App.root}/log" => "rwc",
+    "#{App.root}/public" => "r",
+    "#{App.root}/tmp" => "rwc",
+    "#{App.root}/vendor" => "r",
+    "/var/www/notes/socket" => "rwc",
+    "/usr/local/lib/ruby" => "r",
+  })
+
+  # exclude exec
+  pledges = [
+    "cpath",
+    "dns",
+    "fattr",
+    "flock",
+    "inet",
+    "proc",
+    "prot_exec",
+    "rpath",
+    "stdio",
+    "unix",
+    "wpath",
+  ]
+
+  Pledge.pledge(pledges.join(" "))
+
+  App.logger.info "pledged and unveiled"
+else
+  App.logger.warn "not pledged or unveiled on this platform"
+end
