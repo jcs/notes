802e75afe41cc9af7a5216f058dfd1c79a10ed0a
joshua stein <jcs@jcs.org>
2022-12-21 15:49:38 -0600

    Keystore: Add SQLite support

--- app/models/keystore.rb
+++ app/models/keystore.rb
@@ -10,6 +10,23 @@ class Keystore < DBModel
 
     now = Time.now.strftime("%Y-%m-%d %H:%M:%S")
 
+    if Keystore.connection.adapter_name == "SQLite"
+      Keystore.connection.execute("DELETE FROM #{self.table_name} WHERE " <<
+        "`key` = #{q(key)} AND expiration <= '#{now}'")
+      Keystore.connection.execute("INSERT OR IGNORE INTO " <<
+        "#{self.table_name} (`key`, `value`) VALUES (#{q(key)}, 0)")
+      Keystore.connection.execute("UPDATE #{self.table_name} " <<
+        "SET `value` = `value` + #{q(amount)} WHERE `key` = #{q(key)}")
+
+      if expire_extension > 0
+        Keystore.connection.execute("UPDATE #{self.table_name} SET " <<
+          "expiration = DATETIME(IFNULL(expiration, '#{now}'), " <<
+          "'+#{expire_extension.to_i} second') WHERE `key` = #{q(key)}")
+      end
+
+      return Keystore.where(:key => key).first.try(:value).to_i
+    end
+
     # atomic update and fetch, unless the key is expired then we consider it
     # gone
     self.connection.execute("SET @last_inc := NULL")
@@ -98,105 +115,18 @@ class Keystore < DBModel
   end
 
   def self.put(key, value)
-    self.connection.execute("INSERT INTO #{self.table_name} " +
-      "(`key`, `value`) VALUES (#{q(key)}, #{q(value)}) ON DUPLICATE KEY " +
-      "UPDATE `value` = #{q(value)}, expiration = NULL")
+    if Keystore.connection.adapter_name == "SQLite"
+      self.connection.execute("INSERT OR REPLACE INTO #{self.table_name} " +
+        "(`key`, `value`, `expiration`) VALUES (#{q(key)}, #{q(value)}, NULL)")
+    else
+      self.connection.execute("INSERT INTO #{self.table_name} " +
+        "(`key`, `value`) VALUES (#{q(key)}, #{q(value)}) ON DUPLICATE KEY " +
+        "UPDATE `value` = #{q(value)}, expiration = NULL")
+    end
   end
 
   def self.del(key)
     self.where(:key => key).delete_all
-  end
-
-  # set add, stored as bracket-surrounded string (so srem can properly delete
-  # values)
-  def self.sadd(key, values = [])
-    if !values.is_a?(Array)
-      values = [ values ]
-    end
-
-    values.each do |v|
-      if !v
-        next
-      end
-
-      vs = "[#{v}]"
-
-      # avoid duplicates by searching for the string first
-      self.connection.execute("INSERT INTO #{self.table_name} " <<
-        "(`key`, `value`) VALUES (#{q(key)}, #{q(vs)}) " <<
-        "ON DUPLICATE KEY " <<
-        "UPDATE `value` = " <<
-        "CONCAT(`value`, IF(LOCATE(#{q(vs)}, `value`) = 0, #{q(vs)}, ''))")
-    end
-
-    true
-  end
-
-  # sadd, but not caring about duplicating already-present values (because
-  # smembers will uniq them anyway)
-  def self.sadd_fast(key, values = [])
-    if !values.is_a?(Array)
-      values = [ values ]
-    end
-
-    vs = values.select{|v| v }.uniq.map{|v| "[#{v}]" }.join("")
-
-    self.connection.execute("INSERT INTO #{self.table_name} " <<
-      "(`key`, `value`) VALUES (#{q(key)}, #{q(vs)}) " <<
-      "ON DUPLICATE KEY " <<
-      "UPDATE `value` = " <<
-      "CONCAT(`value`, #{q(vs)})")
-
-    true
-  end
-
-  # set unique values, stored as bracket-surrounded values
-  def self.smembers(key)
-    ks = self.where(:key => key).first
-    if ks
-      vals = ks.value.to_s.split("][")
-      if vals.length == 0
-        return []
-      else
-        # strip off [ from first one, ] from last one
-        vals[0] = vals[0].gsub(/\A\[/, "")
-        vals[vals.length - 1] = vals.last.gsub(/\]\z/, "")
-        return vals.uniq
-      end
-    else
-      return []
-    end
-  end
-
-  # set delete, stored as bracket-surrounded string
-  def self.srem(key, values = [])
-    if !values.is_a?(Array)
-      values = [ values ]
-    end
-
-    values.each do |v|
-      vs = "[#{v}]"
-
-      self.connection.execute("UPDATE #{self.table_name} SET " <<
-        "`value` = REPLACE(`value`, #{q(vs)}, '') WHERE `key` = #{q(key)}")
-    end
-
-    true
-  end
-
-  # set set, stored as bracket-surrounded string
-  def self.sset(key, values = [])
-    if !values.is_a?(Array)
-      values = [ values ]
-    end
-    values = values.map{|v| "[#{v}]" }.uniq.join("")
-
-    self.connection.execute("INSERT INTO #{self.table_name} " <<
-      "(`key`, `value`) VALUES (#{q(key)}, #{q(values)}) " <<
-      "ON DUPLICATE KEY " <<
-      "UPDATE `value` = #{q(values)}")
-
-    true
   end
 
   # sql dates are in local time because everything uses NOW() instead of ruby
